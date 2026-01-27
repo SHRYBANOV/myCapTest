6d7ec0a4a7df54653e719de7d318c6c629a94faf
Serhii Hrybanov <shrybanov@sapi-tech.com>
2026-01-27 07:15:48 +0000

    set38

--- Vacantions/srv/samples.js
+++
@@ -1,3 +0,0 @@
-this.before('READ', Employee, async(req) => {});
-this.on('READ', Employee, async(req, next) => { return next()});
-this.after('READ', Employee, async(employee, req) => {});
--- Vacantions/srv/service.js
+++ Vacantions/srv/service.js
@@ -1,12 +1,15 @@
 const cds = require('@sap/cds');
 const { CREATE, UPDATE } = require('@sap/cds/lib/ql/cds-ql');
 
-module.exports = class MyService2 extends cds.ApplicationService{ init()
+module.exports = class MyService extends cds.ApplicationService{ init()
 {
-    const { Employee, EmplManager } = cds.entities('MyService2')
+    const { Employee, EmplManager } = cds.entities('MyService')
 
     this.before (['CREATE','UPDATE'],Employee,async(req) => {
         console.log('Before CREATE UPDATE Employee', req.data);
+    })
+    this.after (['READ'],Employee,async(empl,req) => {
+        console.log('after READ Employee', empl);
     })
 
 };
